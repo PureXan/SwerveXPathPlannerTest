1ba521216573a2e92bb3dc544c339ff7f4ace64e
Aidan Corrales <aidancorrales@aol.com>
2025-02-04 19:05:04 -0600

    I sowy T.T - Aidan

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -2,7 +2,6 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-//Nathan smells like poop stinky every morning
 
 package frc.robot;
 
